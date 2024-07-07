f880be5f51460c583c848769edb6008d4c371ba9
Rajnish Bhagat <rajnishbhagat4@gmail.com>
2024-07-07 16:48:16 +0530

    Card created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import {VideoCard} from "../components/VideoCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-
+      <VideoCard title={"abc | def | ghi "} 
+      image={"thumbnail.jpg"}
+      author={"Rajnish"}
+      timestamp={"2 days ago"}
+      views={"100k"} />
     </div>
   )
 }
